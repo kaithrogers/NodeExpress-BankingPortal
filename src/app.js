7f0e686372eb671c3f264c25ac857e9546bbd18e
Kaith Rogers <krogers@catalyte.io>
2019-07-23 13:21:19 -0600

    Module 2

--- src/app.js
+++ src/app.js
@@ -9,9 +9,46 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', {
+  encoding: 'UTF-8'
+});
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', {
+  encoding: 'UTF-8'
+});
+
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Index'
+    title: 'Account Summary',
+    accounts: accounts
+  });
+})
+
+app.get('/savings', (req, res) => {
+  res.render('account', {
+    account: accounts.savings
+  });
+})
+
+app.get('/checking', (req, res) => {
+  res.render('account', {
+    account: accounts.checking
+  });
+})
+
+app.get('/credit', (req, res) => {
+  res.render('account', {
+    account: accounts.credit
+  });
+})
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    user: users[0]
   });
 })
 
