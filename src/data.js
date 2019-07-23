03d9678614fbcfdf6e647a64c83aca7bcc654db6
Kaith Rogers <krogers@catalyte.io>
2019-07-23 14:05:26 -0600

    Module 4

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const accountData = fs.readFileSync('src/json/accounts.json', {
+  encoding: 'UTF-8'
+});
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', {
+  encoding: 'UTF-8'
+});
+
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+}
+
+module.exports = {
+  accounts,
+  users,
+  writeJSON
+}
